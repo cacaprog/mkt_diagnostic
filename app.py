266356485c62e320b4598d87cadf241f54c82c4e
cacaprog <ccananea@yahoo.com.br>
2024-07-05 20:35:27 -0300

    Update app to bind to Heroku port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -40,4 +41,5 @@ def submit():
     return render_template('result.html', score=score, recommendation=recommendation, follow_up=follow_up)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
